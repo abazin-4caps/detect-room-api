a83966eb590b780f6578a4895567ffe9a18bbdba
abazin-4caps <alexandre.bazin@4-caps.com>
2026-03-03 22:42:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ def detect_wall_categories(paths, page_width, page_height):
             x0, y0 = item[1].x, item[1].y
             x1, y1 = item[2].x, item[2].y
             length = ((x1 - x0) ** 2 + (y1 - y0) ** 2) ** 0.5
-            if 10 <= length <= max_allowed:
+            if 10 <= length <= 500:
                 lengths.append((length, x0, y0, x1, y1))
 
         if not lengths:
